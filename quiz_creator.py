4081f8f361ef8ba9176aa46f9037ae3294e94422
incessmikee <incess.mikee@gmail.com>
2025-04-09 17:31:22 +0800

    wrote instruction and pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,9 @@
+# Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. 
+# Write the collected data to a text file. Ask another question until the user chose to exit.
+
+# start
+# create loop: ask user input for questions, multiple choices, and the correct answer
+# store the input to a new file
+# ask user if they want add another question
+# if no, stop the loop 
+# end
