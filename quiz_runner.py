64abbcd4456b13aa174de4400724ea5a69f2fa0f
incessmikee <incess.mikee@gmail.com>
2025-05-01 01:19:57 +0800

    add instructions

---
+++ quiz_runner.py
@@ -0,0 +1,5 @@
+# Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
+# Hint: 
+# Use your creativity.
+# Feel free to use any library
+
