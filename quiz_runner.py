d9f204b032c744acd0f5910d023ded3b36c9ce32
incessmikee <incess.mikee@gmail.com>
2025-05-04 12:01:16 +0800

    update pseudocode: add lib PyQt5 for GUI with timer, progress bar

--- quiz_runner.py
+++ quiz_runner.py
@@ -3,10 +3,10 @@
 # Use your creativity.
 # Feel free to use any library
 
-# import both libraries json and random
-# open file with questions
-# return file
-# ask for user's answers
-# run quiz using lib random
-# create for loop for every question asked
+# import both libraries json, random, PyQt5 for GUI
+# create widget to organize quiz and GUI
+# create GUI with progress bar, timer, questions, multiple choice
+# shuffle ques using lib random
+# use check_answer to check if user answer is right/wrong
+# create popups for results every ques
 # define main 
