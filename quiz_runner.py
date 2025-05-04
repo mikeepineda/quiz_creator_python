78e3bd2a3097e8a11eac7f5a4c8505e282b84976
incessmikee <incess.mikee@gmail.com>
2025-05-04 13:04:55 +0800

    create window, set up data, prepare interface, load first question

--- quiz_runner.py
+++ quiz_runner.py
@@ -3,15 +3,9 @@
 # Use your creativity.
 # Feel free to use any library
 
+
+
 # import both libraries json, random, PyQt5 for GUI
-# create widget to organize quiz and GUI
-# create GUI with progress bar, timer, questions, multiple choice
-# shuffle ques using lib random
-# use check_answer to check if user answer is right/wrong
-# create popups for results every ques
-# define main 
-
-
 import sys
 import json
 import random 
@@ -21,3 +15,20 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import Qt, QTimer, QPropertyAnimation, QRect
 
+# create widget to organize quiz and GUI
+class QuizApp(QWidget): 
+    def __init__(self, questions):
+        super().__init__    #initialize
+        self.questions = random.sample(questions, len(questions))
+        self.current_q = 0
+        self.score = 0
+        self.time_per_question = 15 #seconds
+        self.timer = QTimer()
+        self.remaining_time = self.time_per_question  #track how much time left
+
+
+# create GUI with progress bar, timer, questions, multiple choice
+# shuffle ques using lib random
+# use check_answer to check if user answer is right/wrong
+# create popups for results every ques
+# define main 
